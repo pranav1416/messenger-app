1c6a7e8e194e8e429605c080058b1ed2d50ab7ae
Pranav Borole <axel@Pranavs-MacBook-Pro.local>
2021-07-21 20:11:30 -0700

    Fix enhancement: Messages viewing order
    Bug: Order of messages in the chat is not as expected (jumbled).
    Earlier Fix: Applied sorting on messages array before mapping and rendering. Sorted the array using createdAt timestamp, latest message appears last in the array.
    New Fix Changes:
    - Changed 'order' in api request at route '/api/conversations'. Earlier messages were fetched by DESC order, changed it to ASC to get latest message at the last in the fetched data.
    - Modified lastMessage logic to accomodate above change.
    - Removed the sorting required in earlier fix, since it is no longer needed.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -8,24 +8,20 @@ const Messages = (props) => {
 
   return (
     <Box>
-      {messages
-        .sort((m1, m2) =>
-          new Date(m1.createdAt) > new Date(m2.createdAt) ? 1 : -1
-        )
-        .map((message) => {
-          const time = moment(message.createdAt).format("h:mm");
+      {messages.map((message) => {
+        const time = moment(message.createdAt).format("h:mm");
 
-          return message.senderId === userId ? (
-            <SenderBubble key={message.id} text={message.text} time={time} />
-          ) : (
-            <OtherUserBubble
-              key={message.id}
-              text={message.text}
-              time={time}
-              otherUser={otherUser}
-            />
-          );
-        })}
+        return message.senderId === userId ? (
+          <SenderBubble key={message.id} text={message.text} time={time} />
+        ) : (
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
+        );
+      })}
     </Box>
   );
 };
--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -20,9 +20,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      order: [[Message, "createdAt", "ASC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message, order: ["createdAt", "ASC"] },
         {
           model: User,
           as: "user1",
@@ -43,9 +43,9 @@ router.get("/", async (req, res, next) => {
             },
           },
           attributes: ["id", "username", "photoUrl"],
-          required: false,
-        },
-      ],
+          required: false
+        }
+      ]
     });
 
     for (let i = 0; i < conversations.length; i++) {
@@ -69,7 +69,8 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText =
+        convoJSON.messages[convoJSON.messages.length - 1].text;
       conversations[i] = convoJSON;
     }
 
